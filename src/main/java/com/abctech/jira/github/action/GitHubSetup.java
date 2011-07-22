98da8aaaefef7dfe6577bb72132c32ff21ae01f9
Andreas Marienborg <andreas.marienborg@gmail.com>
2011-07-22 12:31:53 +0700

    Blablabla dep

---
+++ src/main/java/com/abctech/jira/github/action/GitHubSetup.java
@@ -0,0 +1,11 @@
+package com.abctech.jira.github.action;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: andremar
+ * Date: 7/22/11
+ * Time: 9:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GitHubSetup {
+}
